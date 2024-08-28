346a9b5365956f844286474fd302158a05930eff
Latifi010 <amaklatif@gmail.com>
2024-08-28 11:46:02 +0200

    Update index.js

--- UserInput/index.js
+++ UserInput/index.js
@@ -5,5 +5,4 @@ document.getElementById("MySubmit").onclick = function(){
     username = document.getElementById("Mytext").value;
     document.getElementById("myh1").textContent = `hello ${username}`
 
-
 }
